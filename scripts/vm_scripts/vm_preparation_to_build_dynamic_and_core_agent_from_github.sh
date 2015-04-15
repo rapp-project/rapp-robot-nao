81b3f6e8cf2473c4c8b5aa8e5c04a7ea60b9d832
Maksym <maksym.figat44@gmail.com>
2015-04-15 21:01:53 +0200

    update of script

--- scripts/vm_scripts/vm_preparation_to_build_dynamic_and_core_agent_from_github.sh
+++ scripts/vm_scripts/vm_preparation_to_build_dynamic_and_core_agent_from_github.sh
@@ -2,16 +2,24 @@
 
 # written by Maksym Figat
 GIT_DIRECTORY=/home/nao/download/git
+WS_RAPP_DIRECTORY=/home/nao/ws_rapp/src
 
 if [ -d $GIT_DIRECTORY ]; then #If direcotory exists
   echo "Removing - a directory. Cleaning git folder."
   rm $GIT_DIRECTORY -rf
 fi
 
+if [ -d $WS_RAPP_DIRECTORY ]; then #If direcotory exists
+  echo "ws_rapp/src exists"
+else
+  echo "Creating - a directory. Creating ~/ws_rapp/src folder."
+  mkdir -p $WS_RAPP_DIRECTORY 
+fi
+
 mkdir -p ~/download/git
 cd ~/download/git
 git clone https://github.com/rapp-project/rapp-robot-nao.git
-cp -r ~/download/git/rapp-robot-nao/scripts/vm_scripts/ ~/scripts
-cp -r ~/download/git/rapp-robot-nao/rapp_robot_agent ~/ws_rapp/src/
-cp -r ~/download/git/rapp-robot-nao/rapp_libraries ~/ws_rapp/src/ #change to RAPP API
-cp -r ~/download/git/rapp-robot-nao/rapp_dynamic ~/ws_rapp/src/
+cp -r ~/download/git/rapp-robot-nao/scripts/vm_scripts/* ~/scripts
+cp -r ~/download/git/rapp-robot-nao/rapp_robot_agent ~/ws_rapp/src
+cp -r ~/download/git/rapp-robot-nao/rapp_libraries ~/ws_rapp/src #change to RAPP API
+cp -r ~/download/git/rapp-robot-nao/rapp_dynamic ~/ws_rapp/src
