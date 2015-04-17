743978def45a5273a43fd178fc6c708b0625669e
Maksym <maksym.figat44@gmail.com>
2015-04-17 15:55:24 +0200

    creating correct structure for automatic generation of hz package

--- scripts/vm_scripts/vm_preparation_to_build_dynamic_and_core_agent_from_github.sh
+++ scripts/vm_scripts/vm_preparation_to_build_dynamic_and_core_agent_from_github.sh
@@ -3,35 +3,36 @@
 # written by Maksym Figat
 GIT_DIRECTORY=/home/nao/download/git
 WS_RAPP_DIRECTORY=/home/nao/ws_rapp/src
+HZ_DIRECTORY=/home/nao/rapp/hz_template/template_package
 
 if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <flag>"
-  echo "flag = 0 - create structure of folders and copy files from downloaded repository"
-  echo "flag = 1 - download rapp-robot-nao repository, create structure of folders and copy files to downloaded repository"
-  exit 1
+	echo "Usage: $0 <flag>"
+	echo "flag = 0 - create structure of folders and copy files from downloaded repository"
+	echo "flag = 1 - download rapp-robot-nao repository, create structure of folders and copy files to downloaded repository"
+	exit 1
 fi
 
 if [ -d $GIT_DIRECTORY ]; then #If direcotory exists
-  if [ $1 -eq 1 ]; then #clone from github
-    echo "Removing - a directory. Cleaning git folder."
-    rm $GIT_DIRECTORY -rf
-  fi
+	if [ $1 -eq 1 ]; then #clone from github
+		echo "Removing - a directory. Cleaning git folder."
+		rm $GIT_DIRECTORY -rf
+	fi
 fi
 
 if [ -d $WS_RAPP_DIRECTORY ]; then #If direcotory exists
-  echo "ws_rapp/src exists"
+	echo "ws_rapp/src exists"
 else
-  echo "Creating - a directory. Creating ~/ws_rapp/src folder."
-  mkdir -p $WS_RAPP_DIRECTORY
-  cd $WS_RAPP_DIRECTORY
-  ~/my_workspace/src/catkin/bin/catkin_init_workspace
+	echo "Creating - a directory. Creating ~/ws_rapp/src folder."
+	mkdir -p $WS_RAPP_DIRECTORY
+	cd $WS_RAPP_DIRECTORY
+	~/my_workspace/src/catkin/bin/catkin_init_workspace
 fi
 
 if [ $1 -eq 1 ]; then #clone from github
-  echo "Clonning from github"
-  mkdir -p ~/download/git
-  cd ~/download/git
-  git clone https://github.com/rapp-project/rapp-robot-nao.git
+	echo "Clonning from github"
+	mkdir -p ~/download/git
+	cd ~/download/git
+	git clone https://github.com/rapp-project/rapp-robot-nao.git
 fi
 
 echo "Copying scripts to /home/nao/scripts/ directory"
@@ -39,5 +40,10 @@ cp -r ~/download/git/rapp-robot-nao/scripts/vm_scripts/* ~/scripts
 echo "Copying source code to /home/nao/ws_rapp/src/ directory"
 cp -r ~/download/git/rapp-robot-nao/ros_packages/* ~/ws_rapp/src
 
-if [  ]
-
+if [  -d $HZ_DIRECTORY ]; then #if directory exists
+	echo "Directory $HZ_DIRECTORY exists"
+else
+	echo "Creating folder $HZ_DIRECTORY"
+	mkdir -p $HZ_DIRECTORY
+fi
+cp -r ~/download/git/rapp-robot-nao/hz-packages/hz_template/template_package/* $HZ_DIRECTORY
