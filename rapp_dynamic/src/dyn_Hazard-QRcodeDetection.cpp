4e5f99223df6ac796a520d7ed789fdc79a233db0
jfigat <jan.figat@gmail.com>
2015-04-17 15:36:16 +0200

    update - audio

--- rapp_dynamic/src/dyn_Hazard-QRcodeDetection.cpp
+++ rapp_dynamic/src/dyn_Hazard-QRcodeDetection.cpp
@@ -3,10 +3,11 @@
 //#####################
 
 #include "NaoVision.h"
+#include "NaoCommunication.h"
 
 main(int argc, char **argv)
 {
-	//NaoVision NaoVision_(argc,argv);
+	NaoCommunication NAO_Communication(argc,argv);
 	NaoVision NAO_Vision(argc,argv);
 	sensor_msgs::Image frame;
 	cv::Mat robotToCameraMatrix;
@@ -26,7 +27,7 @@ main(int argc, char **argv)
 	check = true;
 	while (check==true)
 {
-	frame = NAO_Vision.captureImage("top - adaptive auto exposure 1"/*Auto Exposure Algorithm*/,3/*camera resolution*/);//for hightlights --2; weighted average scene Brightness--1; Adaptive weighted auto exposure for lowlights--3;; 3-4VGA,2-VGA,1-QVGA (camera resolution)
+	frame = NAO_Vision.captureImage("top - adaptive auto exposure 1"/*Auto Exposure Algorithm*/,2/*camera resolution*/);//for hightlights --2; weighted average scene Brightness--1; Adaptive weighted auto exposure for lowlights--3;; 3-4VGA,2-VGA,1-QVGA (camera resolution)
 	robotToCameraMatrix.release();//clears the robotToCameraMatrix
 	robotToCameraMatrix = NAO_Vision.getTransform("CameraTop", 2); //From Camera to waist (robot) coordinate system ; FRAME_TORSO = 0, FRAME_WORLD = 1, FRAME_ROBOT = 2
 	if (frame.height == 0 || frame.width == 0) //frame is empty
@@ -50,9 +51,9 @@ main(int argc, char **argv)
 			text_message = QRcodeDetectionStruct.QRmessage[i].substr(0,found);
 			//std::cout<<text_message;
 		
-			//NAO_Vision.textToSpeech( QRcodeDetectionStruct.QRmessage[i] );//reads the QRcode message
-		//	NAO_Vision.textToSpeech("Q R code detected ");
-		//	NAO_Vision.textToSpeech( text_message );//reads the QRcode message
+			//NAO_Vision.textToSpeech( QRcodeDetectionStruct.QRmessage[i], "English" );//reads the QRcode message
+		//	NAO_Vision.textToSpeech("Q R code detected ", "English");
+		//	NAO_Vision.textToSpeech( text_message, "English" );//reads the QRcode message
 			std::cout<<"QRcode message: \t"<<QRcodeDetectionStruct.QRmessage[i]<<std::endl;
 			std::cout<<"QRcode in robot coordinate system:\n"<<QRcodeDetectionStruct.LandmarkInRobotCoordinate[i]<<std::endl;
 		}
@@ -67,8 +68,8 @@ main(int argc, char **argv)
 					text_message = QRcodeHazardStruct.openedObject[i].substr(0,found);
 				else
 					text_message = QRcodeHazardStruct.openedObject[i];
-				//NAO_Vision.textToSpeech( QRcodeHazardStruct.openedObject[i] + std::string(" is open") );
-				NAO_Vision.textToSpeech( text_message + std::string(" is open") );
+				//NAO_Vision.textToSpeech( QRcodeHazardStruct.openedObject[i] + std::string(" is open"), "English" );
+				NAO_Communication.textToSpeech( text_message + std::string(" is open") , "English");
 				std::cout<<"Detected that "<<QRcodeHazardStruct.openedObject[i]<<" is open"<<std::endl;
 				std::cout<<"Position of detected hazard:\n\t"<<QRcodeHazardStruct.hazardPosition_x[i]<<",\t"<<QRcodeHazardStruct.hazardPosition_y[i]<<std::endl;
 			}
--- rapp_dynamic/src/dyn_QRcodeDetection.cpp
+++ rapp_dynamic/src/dyn_QRcodeDetection.cpp
@@ -3,11 +3,13 @@
 //#####################
 
 #include "NaoVision.h"
+#include "NaoCommunication.h"
 
 main(int argc, char **argv)
 {
 	//NaoVision NaoVision_(argc,argv);
 	NaoVision NAO_Vision(argc,argv);
+	NaoCommunication NAO_Communication(argc,argv);
 	sensor_msgs::Image frame;
 	cv::Mat robotToCameraMatrix;
 	std::vector<double> R00,R10,R20,R21,R22; //vectors containing the values of the corresponding cells of the matrix
@@ -19,7 +21,7 @@ main(int argc, char **argv)
 	// Create a zbar reader
 	zbar::ImageScanner scanner;
 	
-	frame = NAO_Vision.captureImage("top - adaptive auto exposure 1"/*Auto Exposure Algorithm*/, 3/*camera resolution*/);//for hightlights --2; weighted average scene Brightness--1; Adaptive weighted auto exposure for lowlights--3;; 3-4VGA,2-VGA,1-QVGA (camera resolution)
+	frame = NAO_Vision.captureImage("top - adaptive auto exposure 1"/*Auto Exposure Algorithm*/, 2/*camera resolution*/);//for hightlights --2; weighted average scene Brightness--1; Adaptive weighted auto exposure for lowlights--3;; 3-4VGA,2-VGA,1-QVGA (camera resolution)
 	robotToCameraMatrix = NAO_Vision.getTransform("CameraTop", 2); //From Camera to waist (robot) coordinate system; FRAME_TORSO = 0, FRAME_WORLD = 1, FRAME_ROBOT = 2
 	if (frame.height == 0 || frame.width == 0) //frame is empty
 		std::cout<<"Camera frame is empty"<<std::endl;
@@ -49,6 +51,8 @@ main(int argc, char **argv)
 			std::cout<<"QRcode message: \t"<<QRcodeDetectionStruct.QRmessage[i]<<std::endl;
 			std::cout<<"QRcode in robot coordinate system:\n"<<QRcodeDetectionStruct.LandmarkInRobotCoordinate[i]<<std::endl;
 			std::cout<<"rotation of QR-code around OZ axis in robot frame: "<<euler_z[i]*180.f/M_PI<<" in degrees"<<std::endl;
+			//NAO_Vision.textToSpeech( QRcodeDetectionStruct.QRmessage[i], "English" );//reads the QRcode message
+			NAO_Communication.textToSpeech( QRcodeDetectionStruct.QRmessage[i], "English" );//reads the QRcode message
 
 		}
 	}
