25e5696ebd875f0b76dbb83d439d4c7967b443e7
Maksym <maksym.figat44@gmail.com>
2015-04-20 15:42:46 +0200

    scripts needed for sending core agent from vm to nao robot

---
+++ scripts/vm_scripts/rapp_tar_packages.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# written by Maksym Figat
+
+cd /home/nao/ws_rapp
+tar czvf rapp_core_agent.tar.gz install_isolated
--- scripts/vm_scripts/send_packages_nao.sh
+++ scripts/vm_scripts/send_packages_nao.sh
@@ -2,9 +2,14 @@
 
 # written by Maksym Figat
 
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 <NAO_IP_ADDRESS>"
+	exit 1
+fi
+
 echo "Packing rapp packages /home/nao/ws_rapp/install_isolated/"
-bash /home/nao/script/pack_rapp_packages.sh
-echo "Sending packages to nao 192.168.18.91:/home/nao/download/rapp"
-scp /home/nao/packages/rapp_install_isolated.tar.gz nao@192.168.18.91:/home/nao/download/rapp/
+bash /home/nao/script/rapp_tar_packages.sh
+echo "Sending packages to nao $1:/home/nao/download/"
+scp /home/nao/packages/rapp_install_isolated.tar.gz nao@$1:/home/nao/download/
 
   
