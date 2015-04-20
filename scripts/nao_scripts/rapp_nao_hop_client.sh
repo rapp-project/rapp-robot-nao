a065801d95c121ab2e914aa5cfc38bbcead63e55
Maksym <maksym.figat44@gmail.com>
2015-04-20 14:33:45 +0200

    java script file for rapp store interaction

--- scripts/nao_scripts/rapp_nao_hop_client.sh
+++ scripts/nao_scripts/rapp_nao_hop_client.sh
@@ -3,6 +3,6 @@
 # written by Maksym Figat
 
 cd ~/hop/hop-3.0.0/bin 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/hop/bigloo/lib/bigloo/4.2a/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/hop/hop-3.0.0/lib/hop/3.0.0/
-./hop -v -g ../../store_interaction.js
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/my_workspace/install_isolated/lib/bigloo/4.2a/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/my_workspace/install_isolated/lib/hop/3.0.0/
+./hop -v -g ~/rapp/store_interaction.js
