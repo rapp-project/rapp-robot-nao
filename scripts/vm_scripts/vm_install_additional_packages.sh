9de5a0297f4d362862670b1eb4cd1341f55bc5f3
Maksym <maksym.figat44@gmail.com>
2015-04-22 15:57:50 +0200

    small changes

--- scripts/vm_scripts/vm_install_additional_packages.sh
+++ scripts/vm_scripts/vm_install_additional_packages.sh
@@ -21,18 +21,18 @@ else
 fi
 
 # Yaml-cpp
-cd PROGRAMS_DIRECTORY
+cd $PROGRAMS_DIRECTORY
 echo "Downloading source code of yaml-cpp"
 wget https://launchpad.net/ubuntu/+archive/primary/+files/yaml-cpp_0.5.1.orig.tar.gz
 tar zxvf yaml-cpp_0.5.1.orig.tar.gz
 cd yaml-cpp-0.5.1
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=/home/nao/my_workspace/install_isolated
+cmake .. -DCMAKE_INSTALL_PREFIX=$MY_WORKSPACE_INSTALL_ISOLATED
 make install
 
 # Bigloo
-cd PROGRAMS_DIRECTORY
+cd $PROGRAMS_DIRECTORY
 echo "Downloading source code of Bigloo"
 wget ftp://ftp-sop.inria.fr/indes/fp/Bigloo/bigloo4.2a-alpha31Mar15.tar.gz
 tar zxvf bigloo4.2a-alpha31Mar15.tar.gz
@@ -41,7 +41,7 @@ cd bigloo4.2a
 make install
 
 # Hop
-cd PROGRAMS_DIRECTORY
+cd $PROGRAMS_DIRECTORY
 echo "Downloading source code of Hop"
 wget ftp://ftp-sop.inria.fr/indes/fp/Hop/hop-3.0.0-pre14.tar.gz
 tar zxvf hop-3.0.0-pre14.tar.gz
@@ -50,7 +50,7 @@ cd hop-3.0.0-pre14
 make install
 
 # Gsasl
-cd PROGRAMS_DIRECTORY
+cd $PROGRAMS_DIRECTORY
 echo "Downloading source code of Gsasl"
 wget ftp://ftp.gnu.org/gnu/gsasl/libgsasl-1.8.0.tar.gz
 tar zxvf libgsasl-1.8.0.tar.gz
@@ -59,25 +59,25 @@ cd libgsasl-1.8.0
 make install
 
 # Vmime
-cd PROGRAMS_DIRECTORY
+cd $PROGRAMS_DIRECTORY
 echo "Downloading source code of libvmime library"
 wget http://sourceforge.net/projects/vmime/files/vmime/0.9/libvmime-0.9.1.tar.bz2
 tar -xjf libvmime-0.9.1.tar.bz2
 ./configure --prefix=/home/nao/my_workspace/install_isolated
 make install
 
-cd MY_WORKSPACE_SRC_DIR
-echo "Downloading source code from image_common repository
+cd $MY_WORKSPACE_SRC_DIR
+echo "Downloading source code from image_common repository"
 git clone https://github.com/ros-perception/image_common.git
-echo "Downloading source code from nodelet_core repository
+echo "Downloading source code from nodelet_core repository"
 git clone https://github.com/ros/nodelet_core.git
-echo "Downloading source code from bond_core repository
+echo "Downloading source code from bond_core repository"
 git clone https://github.com/ros/bond_core.git
-echo "Downloading source code from cmake_modules repository
+echo "Downloading source code from cmake_modules repository"
 git clone https://github.com/ros/cmake_modules.git
-echo "Downloading source code from vision_opencv repository
+echo "Downloading source code from vision_opencv repository"
 git clone https://github.com/ros-perception/vision_opencv.git
-echo "Downloading source code from rosbridge_suite repository
+echo "Downloading source code from rosbridge_suite repository"
 git clone https://github.com/RobotWebTools/rosbridge_suite.git
 
 cd ..
