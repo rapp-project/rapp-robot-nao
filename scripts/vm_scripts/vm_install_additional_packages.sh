507419bedc9114f0122c8d2c477ee2831a9d61ad
Maksym <maksym.figat44@gmail.com>
2015-04-22 17:00:06 +0200

    copying ROS stuff given in a Ros Tutorial

--- scripts/vm_scripts/vm_install_additional_packages.sh
+++ scripts/vm_scripts/vm_install_additional_packages.sh
@@ -87,4 +87,26 @@ echo "Building ros packages"
 $ROS_DIR/src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release
 
 
+#########################################################
+# Copying ROS stuff given in a Ros tutorial
+#########################################################
+cp /usr/lib/liblog4cxx* install_isolated/lib/
+cp -r /usr/include/log4cxx install_isolated/include/
+cp /usr/lib/libapr* install_isolated/lib/
+cp -r /usr/include/apr* install_isolated/include/
+cp /usr/lib/libtinyxml* install_isolated/lib/
+cp /usr/lib/libPoco* install_isolated/lib/
+cp -r /usr/include/Poco* install_isolated/include/
+cp -r /usr/lib/liburdfdom* install_isolated/lib/
+cp -r /usr/include/urdf* install_isolated/include/
+cp -r /usr/lib/libcxsparse* install_isolated/lib/
+cp -r /usr/lib/libcholmod* install_isolated/lib/
+cp -r /usr/include/cholmod* install_isolated/include/
+cp -r /usr/lib/liblz4* install_isolated/lib/
+cp -r /usr/include/lz4* install_isolated/include/
+cp -r /usr/local/lib/libconsole_bridge* install_isolated/lib/
+cp -r /usr/local/include/console_bridge* install_isolated/include/
+cp -r /usr/lib/python2.7/site-packages/* install_isolated/lib/python2.7/site-packages/
+cp /usr/bin/rosversion install_isolated/bin/
+cp /usr/bin/ros* install_isolated/bin/
 
