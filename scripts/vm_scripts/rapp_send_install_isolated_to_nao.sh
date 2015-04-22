6ff4d6266c4aad8cf1e97c9c8f9b6c742f71dd8d
Maksym <maksym.figat44@gmail.com>
2015-04-22 17:03:58 +0200

    Script sends to Nao ROS workspace - installation of ROS

---
+++ scripts/vm_scripts/rapp_send_install_isolated_to_nao.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# written by Maksym Figat
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <NAO address IP>"
+  exit 1
+fi
+
+cd /home/nao/my_workspace/
+
+tar -czvf install_clean.tar.gz install_isolated
+scp install_clean.tar.gz nao@$1:/home/nao/download/
