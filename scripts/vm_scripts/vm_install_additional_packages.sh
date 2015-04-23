8570aad3f3e1b652a67856aefbd00d0071688d45
Maksym <maksym.figat44@gmail.com>
2015-04-23 19:35:21 +0200

    working script - additional ros packages installation

--- scripts/vm_scripts/vm_install_additional_packages.sh
+++ scripts/vm_scripts/vm_install_additional_packages.sh
@@ -8,13 +8,35 @@ MY_WORKSPACE_SRC_DIR=$ROS_DIR"/src"
 MY_WORKSPACE_INSTALL_ISOLATED=$ROS_DIR"/install_isolated"
 SCRIPTS_DIR="/home/nao/scripts"
 
+cd /home/nao/my_workspace
+
 if [ -d $MY_WORKSPACE_INSTALL_ISOLATED ]; then #If directory exists
-	echo "Folder $ROS_DIR/install_isolated exists"
-else
-	echo "Folder $ROS_DIR/install_isolated doesnt exist"
-	echo "Installing ros core packages to $ROS_DIR/install_isolated"
-	bash $SCRIPTS_DIR/vm_ros_install_isolated.sh
+	echo "Removing $ROS_DIR/install_isolated"
+	rm install_isolated devel_isolated build_isolated -rf
 fi
+
+echo "Building ros packages"
+src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release
+
+cp /usr/lib/liblog4cxx* install_isolated/lib/
+cp -r /usr/include/log4cxx install_isolated/include/
+cp /usr/lib/libapr* install_isolated/lib/
+cp -r /usr/include/apr* install_isolated/include/
+cp /usr/lib/libtinyxml* install_isolated/lib/
+cp /usr/lib/libPoco* install_isolated/lib/
+cp -r /usr/include/Poco* install_isolated/include/
+cp -r /usr/lib/liburdfdom* install_isolated/lib/
+cp -r /usr/include/urdf* install_isolated/include/
+cp -r /usr/lib/libcxsparse* install_isolated/lib/
+cp -r /usr/lib/libcholmod* install_isolated/lib/
+cp -r /usr/include/cholmod* install_isolated/include/
+cp -r /usr/lib/liblz4* install_isolated/lib/
+cp -r /usr/include/lz4* install_isolated/include/
+cp -r /usr/local/lib/libconsole_bridge* install_isolated/lib/
+cp -r /usr/local/include/console_bridge* install_isolated/include/
+cp -r /usr/lib/python2.7/site-packages/* install_isolated/lib/python2.7/site-packages/
+cp /usr/bin/rosversion install_isolated/bin/
+cp /usr/bin/ros* install_isolated/bin/
 
 if [ -d $PROGRAMS_DIRECTORY ]; then #If directory exists
 	echo "Folder $PROGRAMS_DIRECTORY exists"
@@ -33,6 +55,32 @@ mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$MY_WORKSPACE_INSTALL_ISOLATED
 make install
+
+cd /home/nao/my_workspace/src
+echo "Downloading source code from bond_core repository"
+git clone https://github.com/ros/bond_core.git
+echo "Downloading source code from cmake_modules repository"
+git clone https://github.com/ros/cmake_modules.git
+cd ..
+
+echo "Building ros packages"
+src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release
+
+
+cd /home/nao/my_workspace/src
+echo "Downloading source code from image_common repository"
+git clone https://github.com/ros-perception/image_common.git
+echo "Downloading source code from nodelet_core repository"
+git clone https://github.com/ros/nodelet_core.git
+echo "Downloading source code from vision_opencv repository"
+git clone https://github.com/ros-perception/vision_opencv.git
+echo "Downloading source code from rosbridge_suite repository"
+git clone https://github.com/RobotWebTools/rosbridge_suite.git
+
+cd ..
+
+echo "Building ros packages"
+src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release
 
 # Bigloo
 cd $PROGRAMS_DIRECTORY
@@ -68,23 +116,4 @@ wget http://sourceforge.net/projects/vmime/files/vmime/0.9/libvmime-0.9.1.tar.bz
 tar -xjf libvmime-0.9.1.tar.bz2
 ./configure --prefix=$MY_WORKSPACE_INSTALL_ISOLATED
 make install
-
-cd $MY_WORKSPACE_SRC_DIR
-echo "Downloading source code from image_common repository"
-git clone https://github.com/ros-perception/image_common.git
-echo "Downloading source code from nodelet_core repository"
-git clone https://github.com/ros/nodelet_core.git
-echo "Downloading source code from bond_core repository"
-git clone https://github.com/ros/bond_core.git
-echo "Downloading source code from cmake_modules repository"
-git clone https://github.com/ros/cmake_modules.git
-echo "Downloading source code from vision_opencv repository"
-git clone https://github.com/ros-perception/vision_opencv.git
-echo "Downloading source code from rosbridge_suite repository"
-git clone https://github.com/RobotWebTools/rosbridge_suite.git
-
-cd ..
-echo "Building ros packages"
-$ROS_DIR/src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release
-
 
