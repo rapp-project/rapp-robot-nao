4d174f1513029aba853411e72a40b5727dcafd94
dudekw <wojciechsbox@gmail.com>
2015-04-24 15:31:35 +0200

    handling 0 QRcodes event

--- rapp_robot_agent/nodes/rapp_vision/adyn_qrcode_server.py
+++ rapp_robot_agent/nodes/rapp_vision/adyn_qrcode_server.py
@@ -436,34 +436,43 @@ class QRcodeDetectionModule(ALModule):
 				
 				if self.QRdetector.detected>0: # if QRcode is found in the image
 					self.isQRcodeFound = True
+					self.numberOfQRcodes = self.QRdetector.detected
+
+					self.message = (self.QRdetector.QRmessage)
+					## Copy matrices to class object for the ros communication		
+					Matrix4x4Camera = Matrix4x4Message()	#LandmarkInCameraCoordinate
+					Matrix4x4Camera.get_values(self.QRdetector.LandmarkInCameraCoordinate,self.numberOfQRcodes)
+
+					Matrix4x4Robot = Matrix4x4Message() #LandmarkInRobotCoordinate
+					Matrix4x4Robot.get_values(self.QRdetector.LandmarkInRobotCoordinate,self.numberOfQRcodes)
+					
+					##WD
+
+					WD_y = np.arctan2(-Matrix4x4Camera.r31[0],np.sqrt(Matrix4x4Camera.r32[0]*Matrix4x4Camera.r32[0]+Matrix4x4Camera.r33[0]*Matrix4x4Camera.r33[0]))		
+
+					cam = np.array([[Matrix4x4Camera.r11[0],Matrix4x4Camera.r12[0],Matrix4x4Camera.r13[0],Matrix4x4Camera.r14[0]],[Matrix4x4Camera.r21[0],Matrix4x4Camera.r22[0],Matrix4x4Camera.r23[0],Matrix4x4Camera.r24[0]],[Matrix4x4Camera.r31[0],Matrix4x4Camera.r32[0],Matrix4x4Camera.r33[0],Matrix4x4Camera.r34[0]],[Matrix4x4Camera.r41[0],Matrix4x4Camera.r42[0],Matrix4x4Camera.r43[0],Matrix4x4Camera.r44[0]]])
+					matrix_90_x = np.array([[np.cos(-WD_y),0,np.sin(-WD_y),0],
+								[0,1,0,0],
+								[-np.sin(-WD_y),0,np.cos(-WD_y),0],
+								[0,0,0,1]])	
+					
+					print "cam: \n",cam	
+					WD_matrix = np.dot(cam,matrix_90_x)
+					WD_matrix =  np.dot(WD_matrix,matrix_90_x)
+					print "WD: \n",WD_matrix
+				else:
+					self.numberOfQRcodes = 0#self.QRdetector.detected
+					self.message = "No QR code have been found"# (self.QRdetector.QRmessage)
+					## Copy matrices to class object for the ros communication		
+					Matrix4x4Camera = Matrix4x4Message()	#LandmarkInCameraCoordinate
+					Matrix4x4Camera.get_values(0,self.numberOfQRcodes)
+					Matrix4x4Robot = Matrix4x4Camera
 		except AttributeError, ex:
 			print "[QRcode server] - Exception AtrributeError = %s" % str(ex)
 		except Exception, ex:
 			print "[QRcode server] - Unnamed exception = %s" % str(ex)
 			
-		self.numberOfQRcodes = self.QRdetector.detected
-		self.message = (self.QRdetector.QRmessage)
-		## Copy matrices to class object for the ros communication		
-		Matrix4x4Camera = Matrix4x4Message()	#LandmarkInCameraCoordinate
-		Matrix4x4Camera.get_values(self.QRdetector.LandmarkInCameraCoordinate,self.numberOfQRcodes)
 
-		Matrix4x4Robot = Matrix4x4Message() #LandmarkInRobotCoordinate
-		Matrix4x4Robot.get_values(self.QRdetector.LandmarkInRobotCoordinate,self.numberOfQRcodes)
-		
-		##WD
-
-		WD_y = np.arctan2(-Matrix4x4Camera.r31[0],np.sqrt(Matrix4x4Camera.r32[0]*Matrix4x4Camera.r32[0]+Matrix4x4Camera.r33[0]*Matrix4x4Camera.r33[0]))		
-
-		cam = np.array([[Matrix4x4Camera.r11[0],Matrix4x4Camera.r12[0],Matrix4x4Camera.r13[0],Matrix4x4Camera.r14[0]],[Matrix4x4Camera.r21[0],Matrix4x4Camera.r22[0],Matrix4x4Camera.r23[0],Matrix4x4Camera.r24[0]],[Matrix4x4Camera.r31[0],Matrix4x4Camera.r32[0],Matrix4x4Camera.r33[0],Matrix4x4Camera.r34[0]],[Matrix4x4Camera.r41[0],Matrix4x4Camera.r42[0],Matrix4x4Camera.r43[0],Matrix4x4Camera.r44[0]]])
-		matrix_90_x = np.array([[np.cos(-WD_y),0,np.sin(-WD_y),0],
-					[0,1,0,0],
-					[-np.sin(-WD_y),0,np.cos(-WD_y),0],
-					[0,0,0,1]])	
-		
-		print "cam: \n",cam	
-		WD_matrix = np.dot(cam,matrix_90_x)
-		WD_matrix =  np.dot(WD_matrix,matrix_90_x)
-		print "WD: \n",WD_matrix
 		return DetectQRcodesResponse(self.isQRcodeFound, self.numberOfQRcodes, Matrix4x4Camera, Matrix4x4Robot, self.message)
 			
 # Testng SIGINT signal handler
