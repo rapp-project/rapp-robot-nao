c8036e6520d063aa48ec9cb5f99cc3d07aab2584
Maksym <maksym.figat44@gmail.com>
2015-04-28 16:57:07 +0200

    structure of nao

--- scripts/nao_scripts/nao_extract_workspaces.sh
+++ scripts/nao_scripts/nao_extract_workspaces.sh
@@ -2,18 +2,20 @@
 
 # written by Maksym Figat
 
-echo "Unpacking a package in /home/nao/package/rapp"
+echo "Extracting workspaces"
 
 WS_ROS="/home/nao/ws_ros"
 WS_ROS_ADDITIONAL_PACKAGES="/home/nao/ws_ros_additional_packages"
 WS_RAPP_NAO="/home/nao/ws_rapp_nao"
+WS_RAPP_APPLICATIONS_NAO="/home/nao/ws_rapp_applications_nao/install_isolated"
 
 echo "Creates workspaces"
 mkdir -p $WS_ROS
 mkdir -p $WS_ROS_ADDITIONAL_PACKAGES
 mkdir -p $WS_RAPP_NAO
+mkdir -p $WS_RAPP_APPLICATIONS_NAO
 
-echo "Unpackes archived files into correct workspaces"
+echo "Extracts archived files into correct workspaces"
 echo "$WS_ROS"
 cd $WS_ROS
 tar zxvf /home/nao/ws_ros.tar.gz
