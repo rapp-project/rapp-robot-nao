22a3a6a1de22757d54fc8892ce5af4570dd4d420
Maksym <maksym.figat44@gmail.com>
2015-04-28 16:49:44 +0200

    Nao - prepares workspaces

--- scripts/nao_scripts/rapp_unpack_copy_install_isolated.sh
+++ scripts/nao_scripts/rapp_unpack_copy_install_isolated.sh
@@ -3,32 +3,29 @@
 # written by Maksym Figat
 
 echo "Unpacking a package in /home/nao/package/rapp"
-cd /home/nao/ws_rapp
-tar zxvf /home/nao/download/rapp/rapp_install_isolated.tar.gz
 
-: 'echo "Removing lib - core_agent and rapp"
-cd /home/nao/ws_rapp/install_isolated
-rm lib/rapp_core_agent -rf
-rm lib/rapp -rf
+WS_ROS="/home/nao/ws_ros"
+WS_ROS_ADDITIONAL_PACKAGES="/home/nao/ws_ros_additional_packages"
+WS_RAPP_NAO="/home/nao/ws_rapp_nao"
 
-echo "Removing share - core agent and rapp"
-rm share/rapp_core_agent -rf
-rm share/rapp -rf
+echo "Creates workspaces"
+mkdir -p $WS_ROS
+mkdir -p $WS_ROS_ADDITIONAL_PACKAGES
+mkdir -p $WS_RAPP_NAO
 
-echo "Removing include - core agent and rapp"
-rm include/rapp_core_agent -rf
-rm include/rapp -rf
+echo "Unpackes archived files into correct workspaces"
+echo "$WS_ROS"
+cd $WS_ROS
+tar zxvf /home/nao/ws_ros.tar.gz
 
-echo "Copy packages to correct folders"
-cd /home/nao/packages/rapp/install_isolated
-cp -r lib/rapp_core_agent /home/nao/ws_rapp/install_isolated/lib
-cp -r lib/rapp /home/nao/ws_rapp/install_isolated/lib
+echo "$WS_ROS_ADDITIONAL_PACKAGES"
+cd $WS_ROS_ADDITIONAL_PACKAGES
+tar zxvf /home/nao/ws_ros_additional_packages.tar.gz
 
-cp -r share/rapp_core_agent /home/nao/ws_rapp/install_isolated/share
-cp -r share/rapp /home/nao/ws_rapp/install_isolated/share
+echo "$WS_RAPP_NAO"
+cd $WS_RAPP_NAO
+tar zxvf /home/nao/ws_rapp_nao.tar.gz
 
-cp -r include/rapp_core_agent /home/nao/ws_rapp/install_isolated/include
-cp -r include/rapp /home/nao/ws_rapp/install_isolated/include
-
-'
+echo "Removes archived files"
+rm /home/nao/ws_ros.tar.gz /home/nao/ws_ros_additional_packages.tar.gz /home/nao/ws_rapp_nao.tar.gz
 
