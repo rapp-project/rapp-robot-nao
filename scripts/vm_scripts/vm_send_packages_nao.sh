ba94d70855106729e6a20a5465334cfee7b09650
Maksym <maksym.figat44@gmail.com>
2015-06-10 17:47:31 +0200

    Changes

--- scripts/vm_scripts/vm_send_packages_nao.sh
+++ scripts/vm_scripts/vm_send_packages_nao.sh
@@ -7,7 +7,7 @@ if [ "$#" -ne 1 ]; then
 	exit 1
 fi
 
-NAO_SCRIPTS="/home/scripts"
+NAO_SCRIPTS="/home/nao/scripts"
 
 echo "Packing rapp packages /home/nao/ws_rapp/install_isolated/"
 bash /home/nao/scripts/vm_archive_workspaces.sh
@@ -16,13 +16,13 @@ echo "Packing scripts for Nao robot"
 cd /home/nao/ws_rapp_nao/src/rapp-robot-nao/scripts/
 tar czf /home/nao/nao_scripts.tar.gz nao_scripts/
 
-echo "Sending packages to nao $1:/home/nao/"
+echo "Sending packages to nao $1:/home/nao/ - enter password for NAO (nao)"
 scp /home/nao/ws_rapp_nao.tar.gz /home/nao/ws_ros_additional_packages.tar.gz /home/nao/ws_ros.tar.gz nao@$1:/home/nao/
 
-echo "Sending scripts to nao $1:/home/nao/"
+echo "Sending scripts to nao $1:/home/nao/ - enter password for NAO (nao)"
 scp /home/nao/nao_scripts.tar.gz nao@$1:/home/nao/
 
-echo "Sending nao data to nao $1:/home/nao/"
+echo "Sending nao data to nao $1:/home/nao/ - enter password for NAO (nao)"
 scp /home/nap/nao_data.tar.gz nao@$1:/home/nao/
 
 echo "Removing /home/nao/ws_rapp_nao.tar.gz /home/nao/ws_ros_additional_packages.tar.gz"
