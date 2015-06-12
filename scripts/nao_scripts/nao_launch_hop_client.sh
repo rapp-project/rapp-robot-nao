df06781fc9aebf3ce44afb254a3b47ec3c8c1bf3
Maksym <maksym.figat44@gmail.com>
2015-06-12 15:12:42 +0200

    update

--- scripts/nao_scripts/nao_launch_hop_client.sh
+++ scripts/nao_scripts/nao_launch_hop_client.sh
@@ -2,4 +2,4 @@
 
 # written by Maksym Figat
 cd /usr/local/bin 
-./hop -v -g /home/nao/ws_rapp_applications/rapp-applications/nao/hz_packages/store_interaction.js
+./hop -v -g /home/nao/ws_rapp_applications_nao/rapp-applications/nao/hz_packages/store_interaction.js
