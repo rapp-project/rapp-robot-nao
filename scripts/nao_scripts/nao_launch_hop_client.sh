7eed10bc41a296ed23d0a356301bf93d6a2cbbd4
Maksym <maksym.figat44@gmail.com>
2015-06-16 19:57:01 +0200

    update of launch hop script

--- scripts/nao_scripts/nao_launch_hop_client.sh
+++ scripts/nao_scripts/nao_launch_hop_client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # written by Maksym Figat
-cd /usr/local/bin 
-./hop -v -g /home/nao/ws_rapp_applications_nao/rapp-applications/nao/hz_packages/store_interaction.js
+cd /home/nao/ws_ros_additional_packages/install_isolated/bin 
+./hop -v -g /home/nao/ws_rapp_applications_nao/nao/hz_packages/store_interaction.js
