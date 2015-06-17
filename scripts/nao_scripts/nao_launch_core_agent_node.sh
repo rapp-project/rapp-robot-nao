cdb464fe9460f610da4c96e0d880b642490f8243
Maksym <maksym.figat44@gmail.com>
2015-06-17 19:21:55 +0200

    update

--- scripts/nao_scripts/nao_launch_core_agent_node.sh
+++ scripts/nao_scripts/nao_launch_core_agent_node.sh
@@ -2,5 +2,5 @@
 
 # written by Maksym Figat
 
-source /home/nao/ws_ros_additional_packages/install_isolated/setup.bash
+source /home/nao/ws_rapp_nao/install_isolated/setup.bash
 roslaunch rapp_core_agent core_agent.launch --screen
