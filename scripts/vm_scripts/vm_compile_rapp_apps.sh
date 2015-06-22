0cad0b1486222c9b34e1d10190d1611fe27640ec
Maciej Stefanczyk <maciek.slon@gmail.com>
2015-06-22 14:15:07 +0200

    Compile RApps script

---
+++ scripts/vm_scripts/vm_compile_rapp_apps.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# written by Maksym Figat
+
+ESC_SEQ="\x1b["
+COL_GREEN=$ESC_SEQ"32;01m"
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_RED=$ESC_SEQ"31;01m"
+
+PROGRAMS_DIRECTORY="/home/nao/programs"
+
+# ROS - core ROS packages
+WS_ROS_DIR="/home/nao/ws_ros"
+WS_ROS_SRC_DIR=$WS_ROS_DIR"/src"
+WS_ROS_INSTALL_ISOLATED=$WS_ROS_DIR"/install_isolated"
+
+# ROS - additional ROS packages and libraries
+WS_ROS_ADDITIONAL_PACKAGES_DIR="/home/nao/ws_ros_additional_packages"
+WS_ROS_ADDITIONAL_PACKAGES_SRC_DIR=$WS_ROS_ADDITIONAL_PACKAGES_DIR"/src"
+WS_ROS_ADDITIONAL_PACKAGES_ISOLATED=$WS_ROS_ADDITIONAL_PACKAGES_DIR"/install_isolated"
+
+# Rapp - ws_rapp_nao - core agent packages
+WS_RAPP_NAO_DIR="/home/nao/ws_rapp_nao"
+WS_RAPP_NAO_ISOLATED=$WS_RAPP_NAO_DIR"/install_isolated"
+
+# Rapp - ws_rapp_applications_nao - dynamic agent packages
+WS_RAPP_APPLICATIONS_NAO_DIR="/home/nao/ws_rapp_applications_nao"
+
+
+cd $WS_RAPP_APPLICATIONS_NAO_DIR
+echo -e "$COL_GREEN[OK]$COL_RESET - Sources with $WS_RAPP_NAO_ISOLATED"
+source $WS_RAPP_NAO_ISOLATED/setup.bash
+echo -e "$COL_GREEN[OK]$COL_RESET - Compiles workspace: $WS_RAPP_APPLICATIONS_NAO_DIR"
+
+
+if [ "$1" = "-v" ]
+then
+  catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release
+  RES=$?
+else
+  catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release > /tmp/rapp_stdout 2>/tmp/rapp_stderr
+  RES=$?
+fi
+
+if [ $RES != 0 ]
+then
+  echo -e "$COL_RED[ERROR]$COL_RESET - build failed"
+  if [ "$1" != "-v" ]
+  then
+    cat /tmp/rapp_stderr
+  fi
+else
+  echo -e "$COL_GREEN[DONE]$COL_RESET"
+fi
