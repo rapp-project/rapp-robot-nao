e7d6f56b0805b16d651f6c72e5976dc82f0de1a5
Maciej Stefanczyk <maciek.slon@gmail.com>
2015-06-23 13:05:59 +0200

    New node simulating RappStore communication.
    - communicates through request/response topics
    - looks in given directory for already downloaded apps
    - returns path to unpacked app if available

---
+++ rapp_core_agent/nodes/fake_store.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os
+
+import rospy
+from std_msgs.msg import String
+
+class fake_store:
+	def callback(self, data):
+		rospy.loginfo(rospy.get_caller_id() + "New request for %s", data.data)
+		
+		# remove version number and extension from package name
+		base_name = data.data.split('-')[0]
+		path = os.path.join(self.base_path, base_name)
+		
+		# check, whether required package is available
+		if not os.path.exists(path):
+			path = ""
+			rospy.loginfo(rospy.get_caller_id() + "Package not available")
+		else:
+			rospy.loginfo(rospy.get_caller_id() + "Package location: %s", path)
+			
+		# return path to calling process
+		self.pub.publish(str(path))
+
+	def __init__(self):
+		rospy.init_node('fake_store', anonymous=True)
+		
+		self.pub = rospy.Publisher('response', String, queue_size=10)
+		rospy.Subscriber('request', String, self.callback)
+		
+		# read base path from parameter server
+		self.base_path = rospy.get_param('weblets_path', '/home/nao/.config/hop/weblets')
+		
+		rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        fake_store()
+    except rospy.ROSInterruptException:
+        pass
+		
+# install with:
+# catkin_install_python(PROGRAMS fake_store.py
+#   DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION})
