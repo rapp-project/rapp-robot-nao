53781236472038d6749c83baebbd9958947a57d7
nao <nao@virtual-nao.robotyka.ia.pw.edu.pl>
2015-06-26 18:20:18 +0000

    Implementation of feature-based object recognition/detection

--- cloud_services/include/cloud_services/find_objects.hpp
+++ cloud_services/include/cloud_services/find_objects.hpp
@@ -5,7 +5,7 @@
 
 namespace FindObjects {
 	
-	int findObjects(std::string fname, std::vector<std::string> names, std::vector<std::string> files, int limit, 
+	int findObjects(std::string fname, std::vector<std::string> names, std::vector<std::string> files, unsigned int limit, 
 	                std::vector<std::string> & found_names, std::vector<geometry_msgs::Point> & found_centers, std::vector<double> & found_scores);
 	
 };
