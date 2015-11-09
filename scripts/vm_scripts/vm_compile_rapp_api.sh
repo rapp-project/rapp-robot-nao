3666fb71ebcd086b9eb64432c15141551f4789c9
dudekw <wojciechsbox@gmail.com>
2015-11-09 15:03:05 +0100

    rapp_api scripts

--- scripts/vm_scripts/vm_compile_rapp_api.sh
+++ scripts/vm_scripts/vm_compile_rapp_api.sh
@@ -10,8 +10,8 @@ COL_RED=$ESC_SEQ"31;01m"
 # Rapp - ws_rapp_nao - core agent packages
 WS_RAPP_API_DIR="/home/nao/ws_rapp_api"
 cd $WS_RAPP_API_DIR
-mkdir -p src/cpp/build/
-WS_RAPP_API_DIR_BUILD="/home/nao/ws_rapp_api/src/cpp/build/"
+mkdir -p src/rapp-api/cpp/build/
+WS_RAPP_API_DIR_BUILD="/home/nao/ws_rapp_api/src/rapp-api/cpp/build/"
 cd $WS_RAPP_API_DIR_BUILD
 
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/home/nao/ws_rapp_api/install
