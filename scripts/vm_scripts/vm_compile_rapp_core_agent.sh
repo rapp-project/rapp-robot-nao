f371d2ca347643cbd71b49a1fb858da34dd39651
Maksym Figat <maksym.figat44@gmail.com>
2016-05-19 11:24:16 +0200

    Update of core agent script

--- scripts/vm_scripts/vm_compile_rapp_core_agent.sh
+++ scripts/vm_scripts/vm_compile_rapp_core_agent.sh
@@ -26,7 +26,8 @@ if [ ! -f $WS_RAPP_NAO_DIR/src/CMakeLists.txt ]; then
 fi
 
 cd $WS_RAPP_NAO_DIR
-export Rapp_DIR=/home/nao/ws_rapp_api/install/lib/CMake/Rapp
+export Rapp_DIR=/home/nao/ws_rapp_api/install/share/Rapp
+export RappRobots_DIR=/home/nao/ws_rapp_api/install/share/RappRobots
 echo -e "$COL_GREEN[OK]$COL_RESET - Sources with $WS_ROS_ADDITIONAL_PACKAGES_ISOLATED"
 source $WS_ROS_ADDITIONAL_PACKAGES_ISOLATED/setup.bash 
 echo -e "$COL_GREEN[OK]$COL_RESET - Compiles workspace: $WS_RAPP_NAO_DIR"
