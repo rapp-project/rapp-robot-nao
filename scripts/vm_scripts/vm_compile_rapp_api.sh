4a8a94eb273c4f0c555e41fb1ded00ff2c8d44b1
dudekw <wojciechsbox@gmail.com>
2017-01-05 15:22:45 +0100

    installation scripts, clone rapp-api repositories

--- scripts/vm_scripts/vm_compile_rapp_api.sh
+++ scripts/vm_scripts/vm_compile_rapp_api.sh
@@ -7,8 +7,13 @@ COL_GREEN=$ESC_SEQ"32;01m"
 COL_RESET=$ESC_SEQ"39;49;00m"
 COL_RED=$ESC_SEQ"31;01m"
 
-# Rapp - ws_rapp_nao - core agent packages
+# Clone rapp-api repositories 
 WS_RAPP_API_DIR="/home/nao/ws_rapp_api"
+mkdir -p $WS_RAPP_API_DIR/src
+cd $WS_RAPP_API_DIR/src
+git clone https://github.com/rapp-project/rapp-api.git
+git clone https://github.com/rapp-project/rapp-robots-api.git
+# Rapp - ws_rapp_nao - core agent packages
 mkdir -p $WS_RAPP_API_DIR/build/rapp-api
 cd $WS_RAPP_API_DIR/build/rapp-api
 cmake $WS_RAPP_API_DIR/src/rapp-api -DOPEN_NAO=ON -DCMAKE_INSTALL_PREFIX=$WS_RAPP_API_DIR/install
